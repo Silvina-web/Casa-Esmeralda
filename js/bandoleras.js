b7678c87f84d0b21d3ac9189b09a2b5186704183
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-05-31 14:56:34 -0300

    Modal

--- js/bandoleras.js
+++ js/bandoleras.js
@@ -1,24 +1,26 @@
 console.log("bandoleras")
 
 const items= document.getElementById('items-bandoleras')
-
-
+const contenedorCarrito= document.getElementById('carrito-contenedor')
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
-const fetchData = async () => {
+const fetchData= async () => {
      try{
          const res = await fetch ('../Json/bandoleras.json')
          const data = await res.json ()
-         //console.log(data)
+         console.log(data)
          renderizar(data)
        
      } catch (error) {
          console.log (e)
      }
 }
+
+
 const renderizar= data => {
+     let carrito=[]
      data.forEach(element => {
      const div= document.createElement ('div')
      /* div.classList.add('card-bandoleras') */
@@ -27,8 +29,55 @@ const renderizar= data => {
     <h5>${element.nombre}</h5>
     <h6>${element.descripcion}</h6>
     <p>precio:$ ${element.precio}</p>
-
+    <button id="agregar${element.id}" >Agregar</button>
   `
   items.appendChild(div)
+
+  const boton= document.getElementById(`agregar${element.id}`) 
+  boton.addEventListener('click',()=> {
+      agregarCarrito(element.id)
+  })
+  
+     }
+)
+ const agregarCarrito= (prodId) => {
+     const item= data.find(item => item.id === prodId)
+     carrito.push(item)
+     actualizarCarro()
+     console.log(carrito)
  }
-)};
+
+
+const eliminarCarrito= (prodId) => {
+    const item= carrito.find((item) => item.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarro()
+}
+
+
+const actualizarCarro= (item) => {
+    contenedorCarrito.innerHTML= ''
+    carrito.forEach(item => {
+        const div= document.createElement('div')
+        div.innerHTML= `
+        <img src= ${item.img} alt="foto de bandolera"> 
+        <h5>${item.nombre}</h5>
+        <h6>${item.descripcion}</h6>
+        <p>precio:$ ${item.precio}</p>
+        <p>cantidad: <span id="cantidad"> ${item.cantidad}<span></p>
+        <button id="eliminar${item.id}" >Eliminar</button>
+        `
+        contenedorCarrito.appendChild(div)
+        const boton= document.getElementById(`eliminar${item.id}`)
+        boton.addEventListener('click',()=> {
+            eliminarCarrito(item.id)
+        })
+    })
+
+
+
+
+
+}
+}
