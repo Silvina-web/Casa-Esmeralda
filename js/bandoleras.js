aa71d80dfa80e906ec1e2e768a69da733667c52c
Silvina Marcantoni <silvina.marcantoni@gmail.com>
2022-06-01 14:55:10 -0300

    carrito

--- js/bandoleras.js
+++ js/bandoleras.js
@@ -2,12 +2,35 @@ console.log("bandoleras")
 
 const items= document.getElementById('items-bandoleras')
 const contenedorCarrito= document.getElementById('carrito-contenedor')
+const vaciarCarrito= document.getElementById('vaciar-carrito')
+const contadorCarrito = document.getElementById('contadorCarrito') /* ubicado en el head de las secciones, no del home */
+const precioTotal= document.getElementById('precioTotal') /* ubicado en el modal */
+
+
+vaciarCarrito.addEventListener('click',()=> {
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+    contadorCarrito.innerText= 0
+})
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+   
+  
+  
 })
+
+
 const fetchData= async () => {
      try{
+        
          const res = await fetch ('../Json/bandoleras.json')
          const data = await res.json ()
          console.log(data)
@@ -16,11 +39,14 @@ const fetchData= async () => {
      } catch (error) {
          console.log (e)
      }
+     
 }
 
 
+
 const renderizar= data => {
-     let carrito=[]
+
+ let carrito=[]
      data.forEach(element => {
      const div= document.createElement ('div')
      /* div.classList.add('card-bandoleras') */
@@ -29,9 +55,17 @@ const renderizar= data => {
     <h5>${element.nombre}</h5>
     <h6>${element.descripcion}</h6>
     <p>precio:$ ${element.precio}</p>
-    <button id="agregar${element.id}" >Agregar</button>
+    <button id="agregar${element.id}" class="button-favoritos">  <i class="fas fa-heart"> </i> </button>
   `
   items.appendChild(div)
+
+  
+
+
+  
+  
+
+ 
 
   const boton= document.getElementById(`agregar${element.id}`) 
   boton.addEventListener('click',()=> {
@@ -39,13 +73,24 @@ const renderizar= data => {
   })
   
      }
+
+    
 )
  const agregarCarrito= (prodId) => {
-     const item= data.find(item => item.id === prodId)
+ const existe= carrito.some (item => item.id === prodId)
+    if(existe){
+    
+
+        alert('ya esta en el carrito')
+        
+
+    }
+ const item= data.find(item => item.id === prodId)
      carrito.push(item)
-     actualizarCarro()
+      actualizarCarro()
      console.log(carrito)
  }
+ 
 
 
 const eliminarCarrito= (prodId) => {
@@ -60,6 +105,7 @@ const actualizarCarro= (item) => {
     contenedorCarrito.innerHTML= ''
     carrito.forEach(item => {
         const div= document.createElement('div')
+        div.className =('producto-carrito')	
         div.innerHTML= `
         <img src= ${item.img} alt="foto de bandolera"> 
         <h5>${item.nombre}</h5>
@@ -69,15 +115,36 @@ const actualizarCarro= (item) => {
         <button id="eliminar${item.id}" >Eliminar</button>
         `
         contenedorCarrito.appendChild(div)
+
+  
+        
+      
+    
+        
+
+
         const boton= document.getElementById(`eliminar${item.id}`)
         boton.addEventListener('click',()=> {
             eliminarCarrito(item.id)
         })
+
+      
     })
 
-
-
-
+  
+    contadorCarrito.innerText= carrito.length
+    precioTotal.innerText= carrito.reduce((acc,item)=> acc + item.precio,0)
+    
+   
 
 }
+
 }
+
+
+
+/* vaciarCarrito.addEventListener('click',()=> {
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}) */
